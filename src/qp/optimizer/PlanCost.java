247fdbc58f2538abde9e63e2296cac5d35e40bfe
zhangyongzhe20 <jcddyongzhe@gmail.com>
2021-03-02 22:48:14 +0800

    calculate the cost plan of sort-merge join

--- src/qp/optimizer/PlanCost.java
+++ src/qp/optimizer/PlanCost.java
@@ -146,6 +146,15 @@ public class PlanCost {
             case JoinType.BLOCKNESTED:
                 joincost = 7; /* to force block nested loops join, replace with real calculation later */
                 break;
+            case JoinType.SORTMERGE:
+                // cost of sorting left and right page
+                double sortLeftCost = 2 * leftpages * (1 + Math.ceil(Math.log(Math.ceil((double)(leftpages / numbuff))) / Math.log(numbuff - 1)));
+                double sortRightCost = 2 * rightpages * (1 + Math.ceil(Math.log(Math.ceil((double)(rightpages / numbuff))) / Math.log(numbuff - 1)));
+                // cost of merge join
+                double mergeLeftRight = leftpages + rightpages;
+                // total cost
+                joincost = (long) (sortLeftCost + sortRightCost + mergeLeftRight);
+                break;
             default:
                 System.out.println("join type is not supported");
                 return 0;
