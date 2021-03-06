33839a1678db6e25b301dca4c8105e2929eceaa1
scwaterbear <stephenchua@u.nus.edu>
2021-03-06 21:01:42 +0800

    Adds control flow stubs for Sort in RandomOptimizer

--- src/qp/optimizer/RandomOptimizer.java
+++ src/qp/optimizer/RandomOptimizer.java
@@ -72,6 +72,9 @@ public class RandomOptimizer {
             Operator base = makeExecPlan(((Project) node).getBase());
             ((Project) node).setBase(base);
             return node;
+        } else if (node.getOpType() == OpType.SORT) {
+            // TODO?
+            return node;
         } else {
             return node;
         }
@@ -368,6 +371,10 @@ public class RandomOptimizer {
             return findNodeAt(((Select) node).getBase(), joinNum);
         } else if (node.getOpType() == OpType.PROJECT) {
             return findNodeAt(((Project) node).getBase(), joinNum);
+        } else if (node.getOpType() == OpType.SORT) {
+            // TODO?
+            System.out.println("Please implement RandomOptimizer.java:373");
+            return node;
         } else {
             return null;
         }
@@ -392,6 +399,8 @@ public class RandomOptimizer {
             modifySchema(base);
             ArrayList attrlist = ((Project) node).getProjAttr();
             node.setSchema(base.getSchema().subSchema(attrlist));
+        } else if (node.getOpType() == OpType.SORT) {
+            // TODO?
         }
     }
 }
